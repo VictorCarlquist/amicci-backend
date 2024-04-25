f8c6d62aced1897d2381c2f8ab91606f883e16cf
victor <Victor Carlquist>
2024-04-25 10:37:27 -0300

    Create all views

--- briefing/views.py
+++ briefing/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Briefing, Vendor, Category, Retailer
+from .serializers import BriefingSerializer, VendorSerializer, CategorySerializer, RetailerSerializer
 
-# Create your views here.
+
+class BriefingViewSet(viewsets.ModelViewSet):
+    queryset = Briefing.objects.all()
+    serializer_class = BriefingSerializer
+
+
+class VendorViewSet(viewsets.ModelViewSet):
+    queryset = Vendor.objects.all()
+    serializer_class = VendorSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class RetailerViewSet(viewsets.ModelViewSet):
+    queryset = Retailer.objects.all()
+    serializer_class = RetailerSerializer
+
